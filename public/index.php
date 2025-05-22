6fd6d57bbea9e1ce9bc80a2825d6d89481eec24f
Rafael Leite <rafanleite@gmail.com>
2025-05-21 22:20:19 -0300

    feat: add initial HTML structure with Bootstrap and PHP integration

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../routes.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Reconecta Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="script.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
